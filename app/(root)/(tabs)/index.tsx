087b62a547ce25b13d56fa67b87ae49d1365d28f
Karan <mrkaran2k5@gmail.com>
2024-12-27 20:46:11 +0530

    Components For App

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,22 +1,11 @@
+import { Card } from "@/components/Cards";
 import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { FlatList, SafeAreaView, Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text className="font-bold my-10 font-rubik text-3xl">
-        Welcome to Re-State
-      </Text>
-      <Link href={"/sign-in"}>Sign In</Link>
-      <Link href={"/explore"}>Explore</Link>
-      <Link href={"/profile"}>Profile</Link>
-      <Link href={"/property/1"}>Property</Link>
-    </View>
+    <SafeAreaView>
+      
+    </SafeAreaView>
   );
 }
