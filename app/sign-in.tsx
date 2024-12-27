9376aea40860d36168399ca78e94943df37f785e
Karan <mrkaran2k5@gmail.com>
2024-12-27 16:23:54 +0530

    Auth Integrated

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,10 +1,33 @@
 import icons from "@/constants/icons";
 import images from "@/constants/images";
-import { View, Text, ScrollView, Image, TouchableOpacity } from "react-native";
+import { login } from "@/lib/appwrite";
+import { useGlobalContext } from "@/lib/global-provider";
+import { Redirect } from "expo-router";
+import {
+  View,
+  Text,
+  ScrollView,
+  Image,
+  TouchableOpacity,
+  Alert,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const SignIn = () => {
-  const handleLogin = async () => {};
+  const { refetch, loading, isLogged } = useGlobalContext();
+
+  if (!isLogged && loading) {
+    <Redirect href="/" />;
+  }
+
+  const handleLogin = async () => {
+    const result = await login();
+    if (result) {
+      refetch();
+    } else {
+      Alert.alert("Failed to login");
+    }
+  };
 
   return (
     <SafeAreaView className="bg-white h-full">
@@ -20,7 +43,7 @@ const SignIn = () => {
         />
         <View className="px-10">
           <Text className="text-base text-center uppercase font-rubik text-black-200">
-            Welcome To Real Scout
+            Welcome To Real State
           </Text>
           <Text className="text-3xl font-rubik-bold text-black-300 text-center mt-2">
             Let's Get You Closer To {"\n"}
--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -25,20 +25,20 @@ export async function login() {
       OAuthProvider.Google,
       redirectUri
     );
-    if (!response) throw new Error("Failed to login");
+    if (!response) throw new Error("Failed to login 1");
 
     const browserResult = await openAuthSessionAsync(
       response.toString(),
       redirectUri
     );
-    if (browserResult.type !== "success") throw new Error("Failed to login");
+    if (browserResult.type !== "success") throw new Error("Failed to login 2");
 
     const url = new URL(browserResult.url);
 
     const secret = url.searchParams.get("secret")?.toString();
     const userId = url.searchParams.get("userId")?.toString();
 
-    if (!secret || !userId) throw new Error("Failed to login");
+    if (!secret || !userId) throw new Error("Failed to login 3");
 
     const session = await account.createSession(userId, secret);
     if (!session) throw new Error("Failed to create session");
@@ -60,11 +60,11 @@ export async function logout() {
 
 export async function getUser() {
   try {
-    const user = await account.get();
-    if (user.$id) {
-      const userAvatar = avatar.getInitials(user.name || "");
+    const response = await account.get();
+    if (response.$id) {
+      const userAvatar = avatar.getInitials(response.name || "");
       return {
-        ...user,
+        ...response,
         avatar: userAvatar.toString(),
       };
     }
